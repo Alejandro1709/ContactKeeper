cebbd67886d3ee5e133ec87f40079d8157f87231
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-12-02 16:52:50 -0500

    DONE: USER MODEL

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+const { check, validationResult } = require('express-validator');
 
 // @route   POST api/v1/users
 // @desc    Register a new user
 // @access  Public
 
-router.post('/', (req, res) => {
-  res.send('Register user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 connectDB();
 
+app.use(express.json());
 app.use('/api/v1/users', require('./routes/users'));
 app.use('/api/v1/auth', require('./routes/auth'));
 app.use('/api/v1/contacts', require('./routes/contacts'));
